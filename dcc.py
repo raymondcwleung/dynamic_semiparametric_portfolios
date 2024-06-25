3d9145446c797b6980a1e1d927d7e2fb5c114c6f
Raymond C. W. Leung <42256245+raymondcwleung@users.noreply.github.com>
2024-06-24 20:33:25 -0700

    Refactor

--- dcc.py
+++ dcc.py
@@ -15,6 +15,8 @@ import os
 import pathlib
 import pickle
 
+import pandas as pd
+
 import dataclasses
 from dataclasses import dataclass
 
@@ -55,6 +57,7 @@ from sgt import ParamsZSgt, SimulatedInnovations, loglik_mvar_timevarying_sgt
 # jax.config.update("jax_default_device", jax.devices("cpu")[0])
 jax.config.update("jax_enable_x64", True)  # Should use x64 in full prod
 jax.config.update("jax_debug_nans", True)  # Should disable in full prod
+# jax.config.update("jax_disable_jit", True) # Should disable in full prod
 
 @chex.dataclass
 class ParamsMean:
@@ -532,34 +535,7 @@ def _calc_trajectory_uvar_vol(
     Calculate the trajectory of univariate vol's {\\sigma_{i,t}}
     for all t based on the asymmetric-GARCH(1,1) model
     """
-    num_sample = mat_epsilon.shape[0]
-    dim = mat_epsilon.shape[1]
-
-    # mat_sigma = jnp.empty(shape=(num_sample, dim))
-    # mat_sigma = mat_sigma.at[0].set(vec_sigma_init_t0)
-    #
-    # def _body_fun(tt, mat_sigma):
-    #     vec_sigma_t_minus_1 = mat_sigma[tt - 1, :]
-    #     vec_epsilon_t_minus_1 = mat_epsilon[tt - 1, :]
-    #
-    #     vec_sigma2_t = _calc_asymmetric_garch_sigma2(
-    #         vec_sigma_t_minus_1=vec_sigma_t_minus_1,
-    #         vec_epsilon_t_minus_1=vec_epsilon_t_minus_1,
-    #         vec_omega=vec_omega,
-    #         vec_beta=vec_beta,
-    #         vec_alpha=vec_alpha,
-    #         vec_psi=vec_psi,
-    #     )
-    #     vec_sigma_t = jnp.sqrt(vec_sigma2_t)
-    #
-    #     mat_sigma = mat_sigma.at[tt].set(vec_sigma_t)
-    #     return mat_sigma
-    #
-    # mat_sigma = jax.lax.fori_loop(
-    #     lower=1, upper=num_sample, body_fun=_body_fun, init_val=mat_sigma
-    # )
-
-    def gummy(vec_sigma_t_minus_1, vec_epsilon_t_minus_1):
+    def _func(vec_sigma_t_minus_1, vec_epsilon_t_minus_1):
         vec_sigma2_t = _calc_asymmetric_garch_sigma2(
             vec_sigma_t_minus_1=vec_sigma_t_minus_1,
             vec_epsilon_t_minus_1=vec_epsilon_t_minus_1,
@@ -571,12 +547,11 @@ def _calc_trajectory_uvar_vol(
         vec_sigma_t = jnp.sqrt(vec_sigma2_t)
         return vec_sigma_t, vec_sigma_t
 
-    _, mat_sigma = jax.lax.scan(gummy, init = vec_sigma_init_t0, xs = mat_epsilon[1:, :])
+    _, mat_sigma = jax.lax.scan(_func, init = vec_sigma_init_t0, xs = mat_epsilon)
     mat_sigma = jnp.insert(arr = mat_sigma, obj = 0, values = vec_sigma_init_t0, axis = 0)
+    mat_sigma = jnp.delete(mat_sigma, -1, axis = 0)
 
     return mat_sigma
-
-
 
 
 
@@ -592,63 +567,13 @@ def _calc_trajectory_mvar_cov(
     Calculate the trajectory of multivariate covariances
     {\\Sigma_t} based on the DCC model.
     """
-    num_sample = mat_epsilon.shape[0]
-    dim = mat_epsilon.shape[1]
-
-    # mat_u = jnp.empty(shape=(num_sample, dim))
-    # tns_Q = jnp.empty(shape=(num_sample, dim, dim))
-    # tns_Sigma = jnp.empty(shape=(num_sample, dim, dim))
-
     vec_u_0 = _calc_normalized_unexpected_excess_rtn(
         vec_sigma=mat_sigma[0, :], vec_epsilon=mat_epsilon[0, :]
     )
     mat_Gamma_0 = _calc_mat_Gamma(mat_Q=mat_Q_0)
     mat_Sigma_0 = _calc_mat_Sigma(vec_sigma=mat_sigma[0, :], mat_Gamma=mat_Gamma_0)
 
-    # mat_u = mat_u.at[0].set(vec_u_0)
-    # tns_Q = tns_Q.at[0].set(mat_Q_0)
-    # tns_Sigma = tns_Sigma.at[0].set(mat_Sigma_0)
-
-    def _body_fun(tt, carry):
-        mat_u, tns_Q, tns_Sigma = carry
-
-        # Compute Q_t
-        mat_Q_t = _calc_mat_Q(
-            vec_delta=vec_delta,
-            vec_u_t_minus_1=mat_u[tt - 1, :],
-            mat_Q_t_minus_1=tns_Q[tt - 1, :, :],
-            mat_Qbar=mat_Qbar,
-        )
-
-        # Compute Gamma_t
-        mat_Gamma_t = _calc_mat_Gamma(mat_Q=mat_Q_t)
-
-        # Compute \Sigma_t
-        mat_Sigma_t = _calc_mat_Sigma(vec_sigma=mat_sigma[tt, :], mat_Gamma=mat_Gamma_t)
-
-        # Compute u_t
-        vec_u_t = _calc_normalized_unexpected_excess_rtn(
-            vec_sigma=mat_sigma[tt, :], vec_epsilon=mat_epsilon[tt, :]
-        )
-
-        # Bookkeeping
-        mat_u = mat_u.at[tt].set(vec_u_t)
-        tns_Q = tns_Q.at[tt].set(mat_Q_t)
-        tns_Sigma = tns_Sigma.at[tt].set(mat_Sigma_t)
-
-        return mat_u, tns_Q, tns_Sigma
-
-    # # HACK:
-    # carry = jax.lax.fori_loop(
-    #     lower=1,
-    #     upper=num_sample,
-    #     body_fun=_body_fun,
-    #     init_val=(mat_u, tns_Q, tns_Sigma),
-    # )
-    # # _, _, tns_Sigma = carry
-    # mat_u, tns_Q, tns_Sigma = carry
-
-    def gummy(carry, xs):
+    def _func(carry, xs):
         vec_sigma_t, vec_epsilon_t = xs
         vec_u_t_minus_1, mat_Q_t_minus_1, _ = carry
 
@@ -674,21 +599,14 @@ def _calc_trajectory_mvar_cov(
         carry = (vec_u_t, mat_Q_t, mat_Sigma_t)
         return carry, carry
 
-    _, carry = jax.lax.scan(gummy, init = (vec_u_0, mat_Q_0, mat_Sigma_0), xs = (mat_sigma[1:, :], mat_epsilon[1:, :]))
-    arr_u = carry[0]
-    arr_Q = carry[1]
-    arr_Sigma = carry[2]
+    _, carry = jax.lax.scan(_func, init = (vec_u_0, mat_Q_0, mat_Sigma_0), xs = (mat_sigma[1:, :], mat_epsilon[1:, :]))
+    # mat_u = carry[0]
+    # tns_Q = carry[1]
+    tns_Sigma = carry[2]
 
-    arr_u = jnp.insert(arr_u, 0, vec_u_0, axis = 0)
-    arr_Q = jnp.insert(arr_Q, 0, mat_Q_0, axis = 0)
-    arr_Sigma = jnp.insert(arr_Sigma, 0, mat_Sigma_0, axis = 0)
-
-    # HACK:
-    tns_Sigma = arr_Sigma
-
-    # jnp.all(jnp.equal(arr_u, mat_u))
-    # jnp.all(jnp.equal(arr_Q, tns_Q))
-    # jnp.all(jnp.equal(arr_Sigma, tns_Sigma))
+    # mat_u = jnp.insert(mat_u, 0, vec_u_0, axis = 0)
+    # tns_Q = jnp.insert(tns_Q, 0, mat_Q_0, axis = 0)
+    tns_Sigma = jnp.insert(tns_Sigma, 0, mat_Sigma_0, axis = 0)
 
     return tns_Sigma
 
@@ -755,6 +673,19 @@ def _calc_trajectory_innovations_timevarying_lbda(
     mat_lbda = jax.lax.fori_loop(
         lower=1, upper=num_sample, body_fun=_body_fun, init_val=mat_lbda
     )
+
+
+    _func = vmap(sgt._time_varying_lbda_params, in_axes=[1, 0, 0])
+    def gummy(vec_lbda_t_minus_1, vec_z_t_minus_1):
+        vec_lbda_t = _func(mat_lbda_tvparams, vec_lbda_t_minus_1, vec_z_t_minus_1)
+        return vec_lbda_t, vec_lbda_t
+
+    _, arr_lbda = jax.lax.scan(gummy, init = vec_lbda_init_t0, xs = mat_z[1:, :])
+    arr_lbda = jnp.insert(arr_lbda, 0, vec_lbda_init_t0, axis = 0)
+
+    jnp.equal(arr_lbda, mat_lbda)
+
+    breakpoint()
     return mat_lbda
 
 
@@ -989,7 +920,7 @@ def _simulate_returns(
     return simreturns
 
 
-@jit
+# @jit
 def dcc_sgt_loglik(
     mat_returns: jpt.Float[jpt.Array, "num_sample dim"],
     params_dcc_sgt_garch: ParamsDccSgtGarch,
