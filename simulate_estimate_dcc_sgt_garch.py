28377128cfe3a519081e45dbb7271e1d365ac197
Raymond C. W. Leung <42256245+raymondcwleung@users.noreply.github.com>
2024-06-26 21:55:50 -0700

    Update logging

--- simulate_estimate_dcc_sgt_garch.py
+++ simulate_estimate_dcc_sgt_garch.py
@@ -28,31 +28,27 @@ dir_data_simulations = pathlib.Path().resolve().joinpath("./data/simulations/")
 dir_log_simulations.mkdir(parents=True, exist_ok=True)
 dir_data_simulations.mkdir(parents=True, exist_ok=True)
 
-
+# Logging
+current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    datefmt="%Y-%m-%d %I:%M:%S %p",
+    level=logging.INFO,
+    format="%(levelname)s | %(asctime)s | %(message)s",
+    handlers=[
+        logging.FileHandler(dir_log_simulations.joinpath(f"{current_time}_{num_simulations}_{num_sample}_{dim}.log"), mode="w"),
+        logging.StreamHandler(),
+    ],
+)
 
 for sim in range(num_simulations):
     # Setup
     hashid = uuid.uuid4().hex
     str_id = utils.gen_str_id(num_sample=num_sample, dim=dim, hashid=hashid)
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-
     fn = f"{current_time}_{str_id}"
 
-
-    # Logging
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(
-        datefmt="%Y-%m-%d %I:%M:%S %p",
-        level=logging.INFO,
-        format="%(levelname)s | %(asctime)s | %(message)s",
-        handlers=[
-            logging.FileHandler(dir_log_simulations.joinpath(f"{fn}.log"), mode="w"),
-            logging.StreamHandler(),
-        ],
-    )
     logger.info(f"--- Scenario iter {sim}/{num_simulations} num_sample = {num_sample} and dim = {dim} ---")
-
-
     beg_time = time.perf_counter()
 
     # Simulate returns
