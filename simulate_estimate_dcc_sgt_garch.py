9427ca5a28c3d29148bf56a4db06c7e842bec44c
Raymond C. W. Leung <42256245+raymondcwleung@users.noreply.github.com>
2024-06-26 20:47:24 -0700

    Handle case where NaN is returned in simulation-estimation loop

--- simulate_estimate_dcc_sgt_garch.py
+++ simulate_estimate_dcc_sgt_garch.py
@@ -23,22 +23,6 @@ num_simulations = args.num_simulations
 num_sample = args.num_sample
 dim = args.dim
 
-#################################################################
-## Load hyperparameters
-#################################################################
-with open("./params_simulation.yml", "rb") as f:
-    params_simulation = yaml.safe_load(f)
-
-# num_simulations = params_simulation["num_simulations"]
-# lst_num_sample = params_simulation["lst_num_sample"]
-# lst_dim = params_simulation["lst_dim"]
-#
-#
-# HACK:
-# num_simulations = 2
-#
-
-
 # Directories setup
 dir_log_simulations = pathlib.Path().resolve().joinpath("./logs/simulations/")
 dir_data_simulations = pathlib.Path().resolve().joinpath("./data/simulations/")
@@ -48,7 +32,6 @@ dir_data_simulations.mkdir(parents=True, exist_ok=True)
 
 
 for sim in range(num_simulations):
-
     # Setup
     hashid = uuid.uuid4().hex
     str_id = utils.gen_str_id(num_sample=num_sample, dim=dim, hashid=hashid)
@@ -68,7 +51,7 @@ for sim in range(num_simulations):
             logging.StreamHandler(),
         ],
     )
-    logger.info(f"--- Scenario iter {sim} num_sample = {num_sample} and dim = {dim} ---")
+    logger.info(f"--- Scenario iter {sim}/{num_simulations} num_sample = {num_sample} and dim = {dim} ---")
 
 
     beg_time = time.perf_counter()
@@ -81,17 +64,22 @@ for sim in range(num_simulations):
     # Estimate parameters
     logger.info(f"Begin estimation")
     estimation_res = dcc.calc_estimation_dcc_sgt_garch(num_sample=num_sample, dim = dim, simreturns=simreturns)
-    neg_loglik_val = np.array(estimation_res.neg_loglik_val)
-    logger.info(f"End estimation with at value {neg_loglik_val}")
 
-    # Save results
-    dict_res = {"simreturns" : simreturns, "estimation_res" : estimation_res}
+    # Save only results where optimization was valid
+    if estimation_res.valid_optimization:
+        neg_loglik_val = np.array(estimation_res.neg_loglik_val)
+        dict_res = {"simreturns" : simreturns, "estimation_res" : estimation_res}
+        with open(dir_data_simulations.joinpath(f"{fn}.pkl"), "wb") as f:
+            pickle.dump(dict_res, f)
+
+        logger.info(f"End estimation with at value {neg_loglik_val}")
+    else:
+        logger.info(f"Invalid estimation at simulation {sim}")
+
 
     end_time = time.perf_counter()
     total_time = end_time - beg_time
 
-    with open(dir_data_simulations.joinpath(f"{fn}.pkl"), "wb") as f:
-        pickle.dump(dict_res, f)
     logger.info(f"--- Done DCC-SGT-GARCH simulations and estimation results in {total_time:.3f}s {fn} ---")
 
 
